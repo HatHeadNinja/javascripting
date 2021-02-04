9c38f03ba34baab7bf73194733fee0dc5be12cab
Unfamous Dyson <dougdyson@gmail.com>
2021-02-04 15:54:11 -0800

    clean up code

--- quiz-donuts-revisited.js
+++ quiz-donuts-revisited.js
@@ -25,8 +25,5 @@ var donuts = [
 
 // your code goes here
 donuts.forEach(function(donut){
-
-  // donut represents a single element of donuts array
-  // donut is an object, therefore you can access its properties using a dot notation
   console.log(donut.type+" donuts cost $"+donut.cost+" each");
 });
